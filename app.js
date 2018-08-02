84a5b166268449545a143648bbe17a5070cb216c
victoregovea <victoregovea@gmail.com>
2018-08-02 17:50:56 -0400

    Auth working

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
-
+const session      = require('express-session');
+const MongoStore   = require('connect-mongo')(session);
 
 mongoose.Promise = Promise;
 mongoose
@@ -29,6 +30,27 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: 'get fit',
+  resave: true,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
+
+app.use((req, res, next) => {
+  if (req.session.currentUser) {
+    res.locals.currentUserInfo = req.session.currentUser;
+    res.locals.isUserLoggedIn = true;
+  } else {
+    res.locals.isUserLoggedIn = false;
+  }
+
+  next();
+});
 
 // Express View engine setup
 
@@ -52,7 +74,9 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 
 const index = require('./routes/index');
-app.use('/', index);
+const authRoutes = require('./routes/auth');
 
+app.use('/', index);
+app.use('/', authRoutes);
 
 module.exports = app;
---
+++ routes/auth.js
@@ -0,0 +1,112 @@
+// routes/auth.js
+const express = require('express');
+const bcrypt = require('bcrypt');
+const router = express.Router();
+const User = require('../models/user');
+const bcryptSalt = 10;
+
+
+router.get('/signup', (req, res, next) => {
+  res.render('auth/signup', {
+    errorMessage: ''
+  });
+});
+
+router.get('/login', (req, res, next) => {
+    res.render('auth/login', {
+      errorMessage: ''
+    });
+  });
+
+router.post('/signup', (req, res, next) => {
+    const {name, email, password} = req.body;
+  
+    if (email === '' || password === '') {
+      res.render('auth/signup', {
+        errorMessage: 'Enter both email and password to sign up.'
+      });
+      return;
+    }
+  
+    User.findOne({ email }, '_id', (err, existingUser) => {
+      if (err) {
+        next(err);
+        return;
+      }
+  
+      if (existingUser !== null) {
+        res.render('auth/signup', {
+          errorMessage: `The email ${email} is already in use.`
+        });
+        return;
+      }
+  
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashedPass = bcrypt.hashSync(password, salt);
+    
+      const theUser = new User({
+        name,
+        email,
+        password: hashedPass
+      });
+  
+      theUser.save((err) => {
+        if (err) {
+          res.render('auth/signup', {
+            errorMessage: `Something went wrong. Try again later. + ${err}`,
+          });
+          return;
+        }
+  
+        res.redirect('/');
+      });
+    });
+  });
+
+  router.post('/login', (req, res, next) => {
+    const {email, password} = req.body;
+  
+    if (email === '' || password === '') {
+      res.render('auth/login', {
+        errorMessage: 'Enter both email and password to log in.'
+      });
+      return;
+    }
+  
+    User.findOne({ email }, (err, theUser) => {
+      if (err || theUser === null) {
+        res.render('auth/login', {
+          errorMessage: `There isn't an account with email ${email}.`
+        });
+        return;
+      }
+  
+      if (!bcrypt.compareSync(password, theUser.password)) {
+        res.render('auth/login', {
+          errorMessage: 'Invalid password.'
+        });
+        return;
+      }
+  
+      req.session.currentUser = theUser;
+      res.redirect('/');
+    });
+  });
+
+  router.get('/logout', (req, res, next) => {
+    if (!req.session.currentUser) {
+      res.redirect('/');
+      return;
+    }
+  
+    req.session.destroy((err) => {
+      if (err) {
+        next(err);
+        return;
+      }
+  
+      res.redirect('/');
+    });
+  });
+
+module.exports = router;
